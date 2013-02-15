41a460f02bb48edeb90320f26334df40174cdc7e
Dmitry Chestnykh <dmitry@codingrobots.com>
2013-02-15 12:21:49 +0100

    Fix style and variable naming.

--- goxc.go
+++ goxc.go
@@ -28,38 +28,42 @@ import (
 )
 
 const VERSION = "0.0.4"
-const AMD64 = "amd64"
-const X86 = "386"
-const ARM = "arm"
-const DARWIN = "darwin"
-const LINUX = "linux"
-const FREEBSD = "freebsd"
-const WINDOWS = "windows"
+
+// Platform names.
+const (
+	AMD64   = "amd64"
+	X86     = "386"
+	ARM     = "arm"
+	DARWIN  = "darwin"
+	LINUX   = "linux"
+	FREEBSD = "freebsd"
+	WINDOWS = "windows"
+)
 
 var PLATFORMS = [][]string{
-	[]string{DARWIN, X86},
-	[]string{DARWIN, AMD64},
-	[]string{FREEBSD, X86},
-	[]string{FREEBSD, AMD64},
+	{DARWIN, X86},
+	{DARWIN, AMD64},
+	{FREEBSD, X86},
+	{FREEBSD, AMD64},
 	//tried to add FREEBSD/ARM but not working for me yet. 2013-02-15
-	//[]string{ FREEBSD, ARM },
-	[]string{LINUX, X86},
-	[]string{LINUX, AMD64},
-	[]string{LINUX, ARM},
-	[]string{WINDOWS, X86},
-	[]string{WINDOWS, AMD64},
+	//{ FREEBSD, ARM },
+	{LINUX, X86},
+	{LINUX, AMD64},
+	{LINUX, ARM},
+	{WINDOWS, X86},
+	{WINDOWS, AMD64},
 }
 
 var (
-	flagSet               = flag.NewFlagSet("goxc", flag.ExitOnError)
-	verbose               bool
-	is_help               bool
-	is_version            bool
-	is_buildtoolchain     bool
-	aos                   string
-	aarch                 string
-	artifact_version      string
-	artifacts_destination string
+	flagSet          = flag.NewFlagSet("goxc", flag.ExitOnError)
+	verbose          bool
+	isHelp           bool
+	isVersion        bool
+	isBuildToolchain bool
+	aos              string
+	aarch            string
+	artifactVersion  string
+	artifactsDest    string
 )
 
 func redirectIO(cmd *exec.Cmd) (*os.File, error) {
@@ -91,35 +95,35 @@ func BuildToolchain(goos string, arch string) {
 	}
 	var scriptname string
 	if gohostos == WINDOWS {
-		scriptname = "\\src\\make.bat"
+		scriptname = `\src\make.bat`
 	} else {
 		scriptname = "/src/make.bash"
 	}
 	cmd := exec.Command(goroot + scriptname)
 	cmd.Dir = goroot + string(os.PathSeparator) + "src"
 	cmd.Args = append(cmd.Args, "--no-clean")
-	var cgo_enabled string
+	var cgoEnabled string
 	if goos == gohostos && arch == gohostarch {
 		//note: added conditional in line with Dave Cheney, but this combination is not yet supported.
 		if gohostos == FREEBSD && gohostarch == ARM {
-			cgo_enabled = "0"
+			cgoEnabled = "0"
 		} else {
-			cgo_enabled = "1"
+			cgoEnabled = "1"
 		}
 	} else {
-		cgo_enabled = "0"
+		cgoEnabled = "0"
 	}
 
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "GOOS="+goos)
-	cmd.Env = append(cmd.Env, "CGO_ENABLED="+cgo_enabled)
+	cmd.Env = append(cmd.Env, "CGO_ENABLED="+cgoEnabled)
 	cmd.Env = append(cmd.Env, "GOARCH="+arch)
 	if goos == LINUX && arch == ARM {
 		// see http://dave.cheney.net/2012/09/08/an-introduction-to-cross-compilation-with-go
 		cmd.Env = append(cmd.Env, "GOARM=5")
 	}
 	if verbose {
-		log.Printf("'make.bash' env: GOOS=%s, CGO_ENABLED=%s, GOARCH=%s, GOROOT=%s", goos, cgo_enabled, arch, goroot)
+		log.Printf("'make.bash' env: GOOS=%s, CGO_ENABLED=%s, GOARCH=%s, GOROOT=%s", goos, cgoEnabled, arch, goroot)
 		log.Printf("'make.bash' args: %s", cmd.Args)
 		log.Printf("'make.bash' working directory: %s", cmd.Dir)
 	}
@@ -147,12 +151,12 @@ func BuildToolchain(goos string, arch string) {
 	}
 }
 
-func XCPlat(goos string, arch string, call []string, is_first bool) string {
+func XCPlat(goos string, arch string, call []string, isFirst bool) string {
 	log.Printf("building for platform %s_%s.", goos, arch)
 
 	gopath := os.Getenv("GOPATH")
 	if gopath == "" {
-		log.Printf("GOPATH env variable not set!! Using '.'")
+		log.Printf("GOPATH env variable not set! Using '.'")
 		gopath = "."
 	}
 	gohostos := runtime.GOOS
@@ -160,23 +164,23 @@ func XCPlat(goos string, arch string, call []string, is_first bool) string {
 	if err != nil {
 		log.Printf("Error: %v", err)
 	}
-	app_name := filepath.Base(app_dirname)
+	appName := filepath.Base(app_dirname)
 
-	relative_dir := artifact_version + string(os.PathSeparator) + goos + "_" + arch
+	relativeDir := artifactVersion + string(os.PathSeparator) + goos + "_" + arch
 
-	var out_destination_root string
-	if artifacts_destination != "" {
-		out_destination_root = artifacts_destination
+	var outDestRoot string
+	if artifactsDest != "" {
+		outDestRoot = artifactsDest
 	} else {
 		gobin := os.Getenv("GOBIN")
 		if gobin == "" {
 			gobin = gopath + string(os.PathSeparator) + "bin"
 		}
-		out_destination_root = gobin + string(os.PathSeparator) + app_name + "-xc"
+		outDestRoot = gobin + string(os.PathSeparator) + appName + "-xc"
 	}
 
-	out_dir := out_destination_root + string(os.PathSeparator) + relative_dir
-	os.MkdirAll(out_dir, 0755)
+	outDir := outDestRoot + string(os.PathSeparator) + relativeDir
+	os.MkdirAll(outDir, 0755)
 
 	cmd := exec.Command("go")
 	cmd.Args = append(cmd.Args, "build")
@@ -186,25 +190,25 @@ func XCPlat(goos string, arch string, call []string, is_first bool) string {
 	if goos == WINDOWS {
 		ending = ".exe"
 	}
-	relative_bin_for_markdown := goos + "_" + arch + string(os.PathSeparator) + app_name + ending
-	relative_bin := relative_dir + string(os.PathSeparator) + app_name + ending
-	cmd.Args = append(cmd.Args, out_destination_root+string(os.PathSeparator)+relative_bin)
+	relativeBinForMarkdown := goos + "_" + arch + string(os.PathSeparator) + appName + ending
+	relativeBin := relativeDir + string(os.PathSeparator) + appName + ending
+	cmd.Args = append(cmd.Args, outDestRoot+string(os.PathSeparator)+relativeBin)
 	cmd.Args = append(cmd.Args, ".") //relative to pwd (specified in call[0])
 
 	cmd.Env = os.Environ()
 
-	var cgo_enabled string
+	var cgoEnabled string
 	if goos == gohostos {
-		cgo_enabled = "1"
+		cgoEnabled = "1"
 	} else {
-		cgo_enabled = "0"
+		cgoEnabled = "0"
 	}
 
 	cmd.Env = append(cmd.Env, "GOOS="+goos)
-	cmd.Env = append(cmd.Env, "CGO_ENABLED="+cgo_enabled)
+	cmd.Env = append(cmd.Env, "CGO_ENABLED="+cgoEnabled)
 	cmd.Env = append(cmd.Env, "GOARCH="+arch)
 	if verbose {
-		log.Printf("'go' env: GOOS=%s, CGO_ENABLED=%s, GOARCH=%s", goos, cgo_enabled, arch)
+		log.Printf("'go' env: GOOS=%s, CGO_ENABLED=%s, GOARCH=%s", goos, cgoEnabled, arch)
 		log.Printf("'go' args: %s", cmd.Args)
 		log.Printf("'go' working directory: %s", cmd.Dir)
 	}
@@ -217,38 +221,38 @@ func XCPlat(goos string, arch string, call []string, is_first bool) string {
 			log.Printf("Compiler error: %s", err)
 		} else {
 			log.Printf("Artifact generated OK")
-			report_filename := out_destination_root + string(os.PathSeparator) + artifact_version + string(os.PathSeparator) + "downloads.md"
+			reportFilename := outDestRoot + string(os.PathSeparator) + artifactVersion + string(os.PathSeparator) + "downloads.md"
 			var flags int
-			if is_first {
-				log.Printf("Creating %s", report_filename)
+			if isFirst {
+				log.Printf("Creating %s", reportFilename)
 				flags = os.O_WRONLY | os.O_TRUNC | os.O_CREATE
 			} else {
 				flags = os.O_APPEND | os.O_WRONLY
 
 			}
-			f, err := os.OpenFile(report_filename, flags, 0600)
+			f, err := os.OpenFile(reportFilename, flags, 0600)
 			if err == nil {
 				defer f.Close()
-				if is_first {
-					_, err = fmt.Fprintf(f, "%s downloads (%s)\n------------\n\n", app_name, artifact_version)
+				if isFirst {
+					_, err = fmt.Fprintf(f, "%s downloads (%s)\n------------\n\n", appName, artifactVersion)
 				}
-				_, err = fmt.Fprintf(f, " * [%s %s](%s)\n", goos, arch, relative_bin_for_markdown)
+				_, err = fmt.Fprintf(f, " * [%s %s](%s)\n", goos, arch, relativeBinForMarkdown)
 			}
 			if err != nil {
-				log.Printf("Could not report to '%s': %s", report_filename, err)
+				log.Printf("Could not report to '%s': %s", reportFilename, err)
 			}
 		}
 	}
-	return relative_bin
+	return relativeBin
 }
 
-func help_text() {
+func printHelp() {
 	fmt.Fprint(os.Stderr, "`goxc` [options] <directory_name>\n")
 	fmt.Fprintf(os.Stderr, " Version %s. Options:\n", VERSION)
 	flagSet.PrintDefaults()
 }
 
-func version_text() {
+func printVersion() {
 	fmt.Fprintf(os.Stderr, " goxc version %s\n", VERSION)
 }
 
@@ -258,29 +262,29 @@ func GOXC(call []string) (int, error) {
 		return 1, e
 	}
 	remainder := flagSet.Args()
-	if is_help {
-		help_text()
+	if isHelp {
+		printHelp()
 		return 0, nil
-	} else if is_version {
-		version_text()
+	} else if isVersion {
+		printVersion()
 		return 0, nil
-	} else if is_buildtoolchain {
+	} else if isBuildToolchain {
 		//no need for remaining args
 	} else if len(remainder) < 1 {
-		help_text()
+		printHelp()
 		return 1, nil
 	}
 
-	is_first := true
+	isFirst := true
 	for _, v := range PLATFORMS {
 		if aos == "" || v[0] == aos {
 			if aarch == "" || v[1] == aarch {
-				if is_buildtoolchain {
+				if isBuildToolchain {
 					BuildToolchain(v[0], v[1])
 				} else {
-					XCPlat(v[0], v[1], remainder, is_first)
+					XCPlat(v[0], v[1], remainder, isFirst)
 				}
-				is_first = false
+				isFirst = false
 			}
 		}
 	}
@@ -291,11 +295,11 @@ func main() {
 	log.SetPrefix("[goxc] ")
 	flagSet.StringVar(&aos, "os", "", "Specify OS (linux/darwin/windows). Compiles all by default")
 	flagSet.StringVar(&aarch, "arch", "", "Specify Arch (386/x64/arm). Compiles all by default")
-	flagSet.StringVar(&artifact_version, "av", "latest", "Artifact version (default='latest')")
-	flagSet.StringVar(&artifacts_destination, "d", "", "Destination root directory (default=$GOBIN)")
-	flagSet.BoolVar(&is_buildtoolchain, "t", false, "Build cross-compiler toolchain(s)")
-	flagSet.BoolVar(&is_help, "h", false, "Show this help")
-	flagSet.BoolVar(&is_version, "version", false, "version info")
+	flagSet.StringVar(&artifactVersion, "av", "latest", "Artifact version (default='latest')")
+	flagSet.StringVar(&artifactsDest, "d", "", "Destination root directory (default=$GOBIN)")
+	flagSet.BoolVar(&isBuildToolchain, "t", false, "Build cross-compiler toolchain(s)")
+	flagSet.BoolVar(&isHelp, "h", false, "Show this help")
+	flagSet.BoolVar(&isVersion, "version", false, "version info")
 	flagSet.BoolVar(&verbose, "v", false, "verbose")
 	GOXC(os.Args)
 }
